eeb7e8c7da6a6b3085eaa6db0e243fc64085afb5
Douglas Obara <douglasobara97@gmail.com>
2022-08-19 23:05:31 +0300

    commit tree 39581693932983d5fdf1402d73def2e785f41737

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,44 @@
+# creates and returns and empty array
 def create_an_empty_array
-  
+  []
 end
 
+# creates an array with four elements
 def create_an_array
-  
+  [1, 2, 3, "Douglas"]
 end
 
+# takes arguments of an array and an element and adds that element to the end of the array 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
 
+# takes arguments of an array and an element and adds that element to the start of the array
 def add_element_to_start_of_array(array, element)
-  
+   array.unshift(element) 
 end
 
+# takes in an argument of an array and removes the last element in the array
 def remove_element_from_end_of_array(array)
-  
+  array.pop  
 end
 
+# takes in an argument of an array and removes the first element from the array
 def remove_element_from_start_of_array(array)
-  
+    array.shift 
 end
 
+# takes in an argument of an array and an index number and returns the element stored at that index
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number] 
 end
 
+# takes in an argument of an array and returns the first element stored in the array
 def retrieve_first_element_from_array(array)
-  
+  array.first 
 end
 
+# takes in an argument of an array and returns the last element of that array
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
